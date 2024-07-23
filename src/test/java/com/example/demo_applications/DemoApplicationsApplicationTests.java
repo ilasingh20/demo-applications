82ee6d6e284b253032811f13d371992e3e64902f
ilasingh20 <ns.ilasingh@gmail.com>
2024-07-23 21:06:21 +0530

    coderefactor

---
+++ src/test/java/com/example/demo_applications/DemoApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
